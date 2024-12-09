74b464d58caea5af709fa0f066b513fa4e655729
ericksm248 <147954327+ericksm248@users.noreply.github.com>
2024-12-08 22:51:44 -0500

    Create simple_test.py

---
+++ simple_test.py
@@ -0,0 +1,22 @@
+import os
+
+#este es un comentario simple
+"""
+ hola mundo
+ este es un comentario largo
+ el programa solo es para pruebas
+ 26/05/2022
+"""
+
+def hola():
+    print("prueba fun\n")
+
+tes = ["hola que tal","asd","goal"]
+a,b,c = 2,8,4.9
+#name = input("ingrese el nombre: ")
+#print("Su nombre es: "+name)
+hola()
+tes.reverse()
+print(tes)
+print(str(a)+","+str(b)+","+str(c))
+os.system("pause")
