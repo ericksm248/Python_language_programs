950a9516808238fd4527741bdadb587791e23a9c
ericksm248 <147954327+ericksm248@users.noreply.github.com>
2024-12-21 12:37:56 -0500

    Create class decorator.py

---
+++ class decorator.py
@@ -0,0 +1,16 @@
+def deco_m_i(m):
+    def m_interno(self):
+        print("servimos la comida")
+        m(self)
+        print("termino de comer")
+    return m_interno
+
+class animal:
+    def __init__(self,comida):
+        self.comida = comida
+    @deco_m_i
+    def comer(self):
+        print("el animal comio",self.comida)
+
+fido = animal("croquetas")
+fido.comer()
